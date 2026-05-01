fa746be2bfb47338ac30b20d47fe0ed721bc545a
Juan José Ordoñez Villalobos <49448784+coltonsini@users.noreply.github.com>
2026-05-01 11:18:38 -0500

    Update app routes and component paths
    Add redirect and new task-form routes, and update component import paths. These updates reorganize routing and enable a parameterized task form for editing tasks.

--- mi-todo-app/src/app/app.routes.ts
+++ mi-todo-app/src/app/app.routes.ts
@@ -1,13 +1,20 @@
+// * This are the routes of the application, they are used to navigate between pages. Each route has a path and a component that will be loaded when the path is accessed. The loadComponent function is used to lazy load the component, 
+// * which means that the component will only be loaded when it is needed, improving the performance of the application.
+
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'task-form',
+    loadComponent: () => import('./pages/task-form/task-form.page').then(m => m.TaskFormPage)
+  },
+  {
+    path: 'task-form/:id',
+    loadComponent: () => import('./pages/task-form/task-form.page').then(m => m.TaskFormPage)
   },
 ];
